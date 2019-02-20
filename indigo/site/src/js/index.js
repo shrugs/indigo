9e775ecbc89a7acef3f6dbe135cafe02bb4e5e63
Gavin Atkinson <gvnatknsn@protonmail.com>
2019-02-20 11:07:34 -0800

    add type scales, remove system top level dir, split out utils by css prop

--- indigo/site/src/js/index.js
+++ indigo/site/src/js/index.js
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 // import indigo from './indigo/index.js';
 
 // Banner Heading
 const BH = props => {
-  return (
-    <div className='t6 regular mt8 mb10'>
-      { props.children }
-    </div>
-  )
-}
+  return <div className="t6 regular mt8 mb10">{props.children}</div>;
+};
 
 const Link = props => {
-  return (
-    <div className='t4 regular mb3'>
-      { props.children }
-    </div>
-  )
-}
+  return <div className="t4 regular mb3">{props.children}</div>;
+};
 
 class WelcomePage extends Component {
   render() {
     return (
-      <div className='col'>
-        <div className='rh-auto-sm rh7-md grid bg-blue white ph8 ph32-sm pb8'>
+      <div className="col">
+        <div className="rh-auto rh7-ns grid bg-blue white ph8 ph32-sm pb8">
           <div>
             <BH>Indigo</BH>
             <Link>Approach</Link>
@@ -56,13 +48,10 @@ class WelcomePage extends Component {
             <Link>Github</Link>
             <Link>CDN</Link>
           </div>
-
         </div>
-        <div className='grid bg-white black'>
-
-        </div>
+        <div className="grid bg-white black" />
       </div>
-    )
+    );
   }
 }
 
